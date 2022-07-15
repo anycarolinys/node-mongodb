9ece4901775efa8d5dc61ae0273aa76af33d38bd
Any Caroliny <59451746+anycarolinys@users.noreply.github.com>
2022-07-15 13:41:02 -0300

    Update readDocument.js

--- readDocument.js
+++ readDocument.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
-    const uri = "mongodb+srv://anycaroliny:F9KrLy5GQ0x803qA@cluster0.qs0q1ba.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://anycaroliny:<password>@cluster0.qs0q1ba.mongodb.net/?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
